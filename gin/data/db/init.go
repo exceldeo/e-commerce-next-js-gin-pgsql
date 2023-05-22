2b15c4cc1286d7e9bbe6c696747295810c848c0a
exceldeo <excel.deo99@gmail.com>
2023-05-22 13:30:33 +0700

    feat: add init db

---
+++ gin/data/db/init.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func GetGormConnection() *gorm.DB {
+	godotenv.Load(".env")
+
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPass + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Can't connect to DB!")
+	}
+	pgDB, _ := db.DB()
+
+	pgDB.SetMaxIdleConns(10)
+	pgDB.SetMaxOpenConns(100)
+	pgDB.SetConnMaxIdleTime(5 * time.Minute)
+	pgDB.SetConnMaxLifetime(60 * time.Minute)
+
+	fmt.Println("connection success")
+	// gormDB.AutoMigrate(&model.Product{})
+
+	return db
+}
--- gin/main.go
+++ gin/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"e-commerce/data/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func handleRequests() {
+	db.GetGormConnection()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	// router.Static("/docs", "./dist")
+
+	router.Use(CORSMiddleware())
+
+	log.Println("Running HTTP server at 8080")
+	router.Run("localhost:8080")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	handleRequests()
 }
